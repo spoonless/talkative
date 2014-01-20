04ecdde492d27ae31bf0ec9dd7e1685ed4db69bc
David Gayerie <dagaydevel@free.fr>
2014-01-20 20:30:07 +0100

    Problème sous windows : ArticleRepository.getArticle.getSeasons retourne une liste proxy qui ne fonctionne pas hors d'une transaction JTA

--- src/main/java/epsi/talkative/repository/ArticleRepository.java
+++ src/main/java/epsi/talkative/repository/ArticleRepository.java
@@ -1,5 +1,6 @@
 package epsi.talkative.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -31,6 +32,6 @@ public class ArticleRepository {
 	}
 
 	public List<Comment> getComments(Editor editor, String articleUrl) throws NotFoundException {
-		return getArticle(editor, articleUrl).getComments();
+		return new ArrayList<Comment>(getArticle(editor, articleUrl).getComments());
 	}
 }
